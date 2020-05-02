f750c0061e3cc64dbddcc56ce949c03d23897577
Gerhard Dinhof <geahaad@gmail.com>
2020-05-02 07:57:12 +0200

    fetching real-time data

--- src/services/MembersService.js
+++ src/services/MembersService.js
@@ -3,8 +3,28 @@ import axios from 'axios';
 export class MembersService {
 
     getMembers(){
-        return axios.get('resources/data/members.json')
-                .then(res => res.data.data);
+
+        return axios.get('https://api.teamvegan.at/auth/otp',
+            { 
+                headers: {
+                    'x-api-key': 'Tcdz88F8vKsezgpmS0W0PbziYotHxr'
+                }
+            }).then(res => {
+                const otp = res.data;
+
+                return axios.get('https://api.teamvegan.at/dashboard/members',
+                { 
+                    headers: {
+                        Authorization: `Bearer ${otp}`
+                    }
+                }).then(resList => resList.data);
+
+            });
+
+        
+
+        // return axios.get('resources/data/members.json')
+        //         .then(res => res.data.data);
     }
 
     getCarsSmall() {
