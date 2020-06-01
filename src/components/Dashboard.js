1a05a2b0593353bca1c2ab48d3deeb9e77f74c5c
Gerhard Dinhof <geahaad@gmail.com>
2020-06-01 10:13:35 +0200

    Feature/suspend unsuspend (#5)
    * suspend and unsuspend
    
    * v0.1.1

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React, { Component } from 'react';
 
 import { DataTable } from 'primereact/datatable';
@@ -22,25 +23,42 @@ export class Dashboard extends Component {
                 { label: 'Not Paid', value: 'false' },
             ],
             discourseState: [
-                { label: 'Active', value: 'true' },
-                { label: 'Blocked', value: 'false' },
+                { label: 'Active', value: 'active' },
+                { label: 'Suspended', value: 'suspended' },
                 { label: 'Not signed up', value: 'na' },
-            ]
+            ],
+            selectedDiscourseId: null,
         };
         this.onPaymentStatusChange = this.onPaymentStatusChange.bind(this);
         this.onDiscourseStatusChange = this.onDiscourseStatusChange.bind(this);
-
+        
         this.service = new MembersService();
         this.export = this.export.bind(this);
+        // this.suspend = this.suspend.bind(this);
     }
 
     componentDidMount() {
+        this.setState({ loading: true });
         this.service.getMembers().then(data => this.setState(
             {
                 members: data,
                 loading: false
             }
         ));
+    }
+
+    suspend(userId) {
+        this.loading = true;
+        this.service.suspendMember(userId).then(data => this.setState({
+            loading: false
+        }))
+    }
+
+    unsuspend(userId) {
+        this.loading = true;
+        this.service.unsuspendMember(userId).then(data => this.setState({
+            loading: false
+        }))
     }
 
     export() {
@@ -54,70 +72,87 @@ export class Dashboard extends Component {
 
     onDiscourseStatusChange(event) {
         if (event.value === 'na') {
-            this.dt.filter(null, 'discourse', 'equals');
+            this.dt.filter(null, 'discourse.id', 'equals');
+        } else if (event.value === 'suspended') {
+            this.dt.filter(null, 'discourse.suspended_at', 'equals');
         } else {
-            this.dt.filter(event.value, 'discourse.active', 'equals');
+            this.dt.filter("*.", 'discourse.suspended_at', 'equals');
         }
         this.setState({selectedDiscourseState: event.value});
     }
 
     paymentStatusTemplate(rowData, column) {
         if (rowData['paid'] === true) {
-            return <i className="pi pi-check"></i>
+            return <i className="pi pi-check"/>
         } else {
-            return <i className="pi pi-times"></i>
+            return <i className="pi pi-times"/>
         }
     }
 
     discourseStatusTemplate(rowData, column) {
-        if (rowData['discourse']['active'] === true) {
-            return <i className="pi pi-check"></i>
+        if (!rowData['discourse']['id']) {
+            return <i class="pi pi-user-minus" />
+        } else if (rowData['discourse']['suspended_at']) {
+            return ( 
+                <div>
+                    <i className="pi pi-times"/>
+                    <Button className="p-button-success p-button-raised p-button-rounded" icon="pi pi-unlock" type="button" tooltip="Unsuspend user" onClick={() => this.unsuspend(rowData['discourse']['id'])}/>
+                </div>
+            );
         } else {
-            return <i className="pi pi-times"></i>
+            return (
+                <div>
+                    <i className="pi pi-check"/>
+                    <Button className="p-button-danger p-button-raised p-button-rounded" icon="pi pi-lock" type="button" tooltip="Suspend user" onClick={() => this.suspend(rowData['discourse']['id'])}/>
+                </div>
+            );
         }
     }
 
     render() {
-        // const paginatorLeft = <Button icon="pi pi-refresh"/>;
-        const paymentStateFilter = <Dropdown style={{width: '100%'}} placeholder="filter" 
-            value={this.state.selectedPaymentState} options={this.state.paymentState} onChange={this.onPaymentStatusChange} showClear />;
-        const discourseStateFilter = <Dropdown style={{width: '100%'}} placeholder="filter" 
-            value={this.state.selectedDiscourseState} options={this.state.discourseState} onChange={this.onDiscourseStatusChange} showClear />;
+        const paymentStateFilter =
+            <Dropdown style={{width: '100%'}} placeholder="filter" 
+                value={this.state.selectedPaymentState} options={this.state.paymentState} onChange={this.onPaymentStatusChange} showClear />;
+        const discourseStateFilter =
+            <Dropdown style={{width: '100%'}} placeholder="filter" 
+                value={this.state.selectedDiscourseState} options={this.state.discourseState} onChange={this.onDiscourseStatusChange} showClear />;
 
-        const header = <div style={{textAlign:'left'}}>
-                        <Button type="button" icon="pi pi-external-link" iconPos="left" label="CSV" onClick={this.export}/>
-                    </div>;
+        const header = (
+            <div style={{textAlign:'left'}}>
+                <Button type="button" icon="pi pi-external-link" iconPos="left" label="CSV" onClick={this.export}/>
+            </div>);
 
 
-        const headerGroup = <ColumnGroup>
-                            <Row>
-                                <Column header="name" rowSpan={2} field="name" sortable />
-                                <Column header="email" rowSpan={2} field="email" sortable />
-                                <Column header="paid" rowSpan={2} field="paid" />
-                                <Column header="payment" colSpan={4} />
-                                <Column header="forum" colSpan={2} />
-                            </Row>
-                            <Row>
-                                <Column header="method" field="payment.method" sortable />
-                                <Column header="date" field="payment.date" sortable />
-                                <Column header="amount" field="payment.amount" sortable />
-                                <Column header="payer" field="payment.payerName" sortable />
+        const headerGroup = (
+            <ColumnGroup>
+                <Row>
+                    <Column header="name" rowSpan={2} field="name" sortable />
+                    <Column header="email" rowSpan={2} field="email" sortable />
+                    <Column header="paid" rowSpan={2} field="paid" />
+                    <Column header="payment" colSpan={4} />
+                    <Column header="forum" colSpan={2} />
+                </Row>
+                <Row>
+                    <Column header="method" field="payment.method" sortable />
+                    <Column header="date" field="payment.date" sortable />
+                    <Column header="amount" field="payment.amount" sortable />
+                    <Column header="payer" field="payment.payerName" sortable />
 
-                                <Column header="username" field="discourse.username" sortable />
-                                <Column header="active" field="discourse.active" sortable />
-                            </Row>
-                            <Row>
-                                <Column field="name" filter filterPlaceholder="filter" />
-                                <Column field="email" filter filterPlaceholder="filter" />
-                                <Column field="paid" filter filterElement={paymentStateFilter} />
-                                <Column />
-                                <Column />
-                                <Column />
-                                <Column />
-                                <Column field="discourse.username" filter filterPlaceholder="filter" />
-                                <Column field="discourse" filter filterElement={discourseStateFilter} />
-                            </Row>
-                        </ColumnGroup>;
+                    <Column header="username" field="discourse.username" sortable />
+                    <Column header="status" field="discourse.active" sortable />
+                </Row>
+                <Row>
+                    <Column field="name" filter filterPlaceholder="filter" />
+                    <Column field="email" filter filterPlaceholder="filter" />
+                    <Column field="paid" filter filterElement={paymentStateFilter} />
+                    <Column />
+                    <Column />
+                    <Column />
+                    <Column />
+                    <Column field="discourse.username" filter filterPlaceholder="filter" />
+                    <Column field="discourse" filter filterElement={discourseStateFilter} />
+                </Row>
+            </ColumnGroup>);
 
         return (
             <DataTable 
@@ -145,7 +180,7 @@ export class Dashboard extends Component {
                 <Column field="payment.payerName" />
 
                 <Column field="discourse.username" className="ellipsis" />
-                <Column field="discourse.active" body={this.discourseStatusTemplate} />
+                <Column field="discourse.active" body={this.discourseStatusTemplate.bind(this)} />
             </DataTable>
         );
     }
--- src/services/MembersService.js
+++ src/services/MembersService.js
@@ -2,17 +2,20 @@ import axios from 'axios';
 
 export class MembersService {
 
+    API_HOST = 'https://api-qs.teamvegan.at';
+    API_KEY = 'd31VFwC6AYUHw413iIKzElKGntTT9X';
+
     getMembers(){
 
-        return axios.get('https://api.teamvegan.at/auth/otp',
+        return axios.get(`${this.API_HOST}/auth/otp`,
             { 
                 headers: {
-                    'x-api-key': 'Tcdz88F8vKsezgpmS0W0PbziYotHxr'
+                    'x-api-key': this.API_KEY
                 }
             }).then(res => {
                 const otp = res.data;
 
-                return axios.get('https://api.teamvegan.at/dashboard/members',
+                return axios.get(`${this.API_HOST}/dashboard/members`,
                 { 
                     headers: {
                         Authorization: `Bearer ${otp}`
@@ -21,4 +24,47 @@ export class MembersService {
 
             });
     }
+
+    suspendMember(userId){
+
+        return axios.get(`${this.API_HOST}/auth/otp`,
+            { 
+                headers: {
+                    'x-api-key': this.API_KEY
+                }
+            }).then(res => {
+                const otp = res.data;
+
+                return axios.put(`${this.API_HOST}/discourse/users/suspend?id=${userId}`, null,
+                { 
+                    headers: {
+                        Authorization: `Bearer ${otp}`
+                    }
+                }).then(resList => resList.data);
+
+            });
+
+    }
+
+    unsuspendMember(userId){
+
+        return axios.get(`${this.API_HOST}/auth/otp`,
+            { 
+                headers: {
+                    'x-api-key': this.API_KEY
+                }
+            }).then(res => {
+                const otp = res.data;
+
+                return axios.put(`${this.API_HOST}/discourse/users/unsuspend?id=${userId}`, null,
+                { 
+                    headers: {
+                        Authorization: `Bearer ${otp}`
+                    }
+                }).then(resList => resList.data);
+
+            });
+
+    }
+
 }
