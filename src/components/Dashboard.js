067c70b20eb1cdbdc79a9a43b1558bdc2325500c
Gerhard Dinhof <geahaad@gmail.com>
2022-05-14 06:58:17 +0200

    dob, export, column selection (disabled)

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,6 +7,7 @@ import { Column } from 'primereact/column';
 import { ColumnGroup } from 'primereact/columngroup';
 import { Row } from 'primereact/row';
 import { Dropdown } from 'primereact/dropdown';
+// import { MultiSelect } from 'primereact/multiselect';
 import { Tooltip } from 'primereact/tooltip';
 
 import { MembersService } from '../services/MembersService';
@@ -16,10 +17,37 @@ export class Dashboard extends Component {
 
     constructor() {
         super();
+
+        this.onPaymentStatusChange = this.onPaymentStatusChange.bind(this);
+        this.onSubscriptionStatusChange = this.onSubscriptionStatusChange.bind(this);
+        this.onMembershipYearChange = this.onMembershipYearChange.bind(this);
+        this.service = new MembersService();
+
+        // Export (https://www.primefaces.org/primereact/datatable/export/)
+        this.exportCSV = this.exportCSV.bind(this);
+        this.exportPdf = this.exportPdf.bind(this);
+        this.exportExcel = this.exportExcel.bind(this);
+        this.exportColumns = [
+            { field: 'name', header: 'Name' },
+            { field: 'email', header: 'Email' },
+            { field: 'dob', header: 'Date of birth' },
+            { field: 'paid', header: 'Paid' }
+        ];
+        this.exportColumns = this.exportColumns.map(col => (
+            { title: col.header, dataKey: col.field }
+        ));
+
+        // Column Toggle (https://www.primefaces.org/primereact/datatable/coltoggle/)
+        this.toggleColumns = [
+            { field: 'actions', header: 'actions' },
+            { field: 'dob', header: 'dob' }
+        ];
+        
         this.state = {
             members: null,
             numberOfMembers: 0,
             loading: true,
+            selectedColumns: this.toggleColumns,
             selectedPaymentState: null,
             selectedSubscriptionState: null,
             selectedMembershipYearState: CalcUtil.getCurrentMembershipYear().toString(),
@@ -38,12 +66,8 @@ export class Dashboard extends Component {
             ],
             selectedDiscourseId: null,
         };
-        this.onPaymentStatusChange = this.onPaymentStatusChange.bind(this);
-        this.onSubscriptionStatusChange = this.onSubscriptionStatusChange.bind(this);
-        this.onMembershipYearChange = this.onMembershipYearChange.bind(this);
-        this.service = new MembersService();
-        this.export = this.export.bind(this);
-        this.setState({ loading: true });
+
+        this.onColumnToggle = this.onColumnToggle.bind(this);
     }
 
 
@@ -76,8 +100,47 @@ export class Dashboard extends Component {
         }))
     }
 
-    export() {
+    exportCSV(selectionOnly) {
         this.dt.exportCSV();
+    }
+
+    exportPdf() {
+        import('jspdf').then(jsPDF => {
+            import('jspdf-autotable').then(() => {
+                const doc = new jsPDF.default("landscape", "mm");
+                doc.autoTable(
+                    this.exportColumns,
+                    this.state.members
+                    );
+                doc.save(`members-${this.state.selectedMembershipYearState}_export.pdf`);
+            })
+        })
+    }
+
+    exportExcel() {
+        import('xlsx').then(xlsx => {
+            const worksheet = xlsx.utils.json_to_sheet(this.state.members);
+            const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+            const excelBuffer = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+            this.saveAsExcelFile(excelBuffer, `members-${this.state.selectedMembershipYearState}`);
+        });
+    }
+
+    saveAsExcelFile(buffer, fileName) {
+        import('file-saver').then(FileSaver => {
+            let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+            let EXCEL_EXTENSION = '.xlsx';
+            const data = new Blob([buffer], {
+                type: EXCEL_TYPE
+            });
+            FileSaver.default.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+        });
+    }
+
+    onColumnToggle(event) {
+        let selectedColumns = event.value;
+        let orderedSelectedColumns = this.toggleColumns.filter(col => selectedColumns.some(sCol => sCol.field === col.field));
+        this.setState({ selectedColumns: orderedSelectedColumns });
     }
 
     onPaymentStatusChange(event) {
@@ -243,30 +306,47 @@ export class Dashboard extends Component {
                 value={this.state.selectedSubscriptionState} options={this.state.subscriptionState} onChange={this.onSubscriptionStatusChange} showClear />;
 
         const header = (
-            <div style={{textAlign:'left'}}>
-                <Button type="button" icon="pi pi-external-link" iconPos="left" label="CSV" onClick={this.export}/>
-                <Dropdown 
-                    value={this.state.selectedMembershipYearState} options={this.state.membershipYearState} onChange={this.onMembershipYearChange} />
-                <div className="activemembers">Active members: {this.state.numberOfMembers}</div>
+            <div className="left-50">
+                <div className="flex flex-row flex-wrap card-container blue-container">
+                    <Button type="button" icon="pi pi-file" onClick={() => this.exportCSV(false)} className="p-button-secondary mr-2" data-pr-tooltip="CSV" tooltip="CSV" />
+                    <Button type="button" icon="pi pi-file-excel" onClick={this.exportExcel} className="p-button-success mr-2" data-pr-tooltip="XLS" tooltip="XLS" />
+                    <Button type="button" icon="pi pi-file-pdf" onClick={this.exportPdf} className="p-button-warning  mr-2" data-pr-tooltip="PDF" tooltip="PDF" />
+
+                    {/*<MultiSelect value={this.state.selectedColumns} options={this.toggleColumns} optionLabel="header" onChange={this.onColumnToggle} style={{width:'20em'}}/>*/}
+
+                    <Dropdown 
+                        value={this.state.selectedMembershipYearState} options={this.state.membershipYearState} onChange={this.onMembershipYearChange} />
+                    <div className="activemembers">Active members: {this.state.numberOfMembers}</div>
+                </div>
             </div>);
 
+        // const columnComponents = this.state.selectedColumns.map(col=> {
+        //     return <Column key={col.field} field={col.field} header={col.header} />;
+        // });
+        // const toggleActions = this.state.selectedColumns.map(col => {
+        //     return <Column field="actions" colSpan={3} />
+        // });
 
         const headerGroup = (
             <ColumnGroup>
+
                 <Row>
                     <Column header="name" rowSpan={2} field="name" sortable />
                     <Column header="email" rowSpan={2} field="email" sortable />
-                    <Column header={ `membership ${this.state.selectedMembershipYearState}` } colSpan={3} />
+                    <Column header="dob" rowSpan={2} field="dob" sortable />
+                    <Column header={ `membership ${this.state.selectedMembershipYearState}` } colSpan={2} />
                     <Column header="forum" />
                     <Column header="actions" rowSpan={2} colSpan={3} field="actions" />
                 </Row>
                 <Row>
                     <Column header="paid" field="paid" sortable />
-                    <Column header="dd" field="activeSubscription" sortable />
+                    {/*<Column header="dd" field="activeSubscription" sortable />*/}
+                    {/*<Column header="amount" field="payment.amount" sortable />*/}
                     <Column header="date" field="payment.paidAt" sortable />
                     <Column header="username" field="discourse.username" sortable />
                 </Row>
                 <Row>
+                    
                     <Column field="name"
                             filter
                             filterPlaceholder="filter"
@@ -277,13 +357,21 @@ export class Dashboard extends Component {
                             filterPlaceholder="filter"
                             filterMatchMode="contains" />
 
+                    <Column field="dob"
+                            filter
+                            filterPlaceholder="filter"
+                            filterMatchMode="contains" />
+
                     <Column field="paid"
                             filter
                             filterElement={paymentStateFilter} />
 
-                    <Column field="activeSubscription" 
-                            filter
-                            filterElement={subscriptionStateFilter}  />
+                    {/*<Column field="activeSubscription" */}
+                    {/*        filter*/}
+                    {/*        filterElement={subscriptionStateFilter}  />*/}
+                    
+                    {/*<Column field="payment.amount" */}
+                    {/*        filter /> */}
 
                     <Column field="payment.paidAt"
                             filter
@@ -295,6 +383,7 @@ export class Dashboard extends Component {
                             filterPlaceholder="filter"
                             filterMatchMode="contains" />
 
+                    {/* {toggleActions} */}
                     <Column field="actions" colSpan={3} />
                 </Row>
             </ColumnGroup>);
@@ -304,6 +393,8 @@ export class Dashboard extends Component {
                 value={this.state.members}
                 sortField="payment.paidAt" sortOrder={-1}
                 paginator
+                showGridlines
+                responsiveLayout="scroll"
                 responsive
                 paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
                 currentPageReportTemplate="Showing {first} to {last} of {totalRecords} entries"
@@ -319,9 +410,11 @@ export class Dashboard extends Component {
 
                 <Column field="name" />
                 <Column field="email" className="ellipsis" />
+                <Column field="dob" />
 
                 <Column field="paid" body={this.paymentStatusTemplate} />
-                <Column field="activeSubscription" body={this.subscriptionStatusTemplate} />
+                {/*<Column field="activeSubscription" body={this.subscriptionStatusTemplate} />*/}
+                {/*<Column field="payment.amount" />*/}
                 <Column field="payment.paidAt" body={this.dateTemplate} />
 
                 <Column field="discourse.username" className="ellipsis" />
